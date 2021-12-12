1cbe8a1c85028b61fcb99eacec96addddd5f8d03
JCastanho <jomi.castanho@gmail.com>
2021-12-12 00:32:15 +0000

    scraping team members not working yet!

--- scraper.py
+++ scraper.py
@@ -2,14 +2,99 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-url = "https://desporto.sapo.pt/futebol/competicao/primeira-liga-2/classificacao"
+url_base = "https://desporto.sapo.pt"
+url = url_base + "/futebol/competicao/primeira-liga-2/classificacao"
+
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, "html.parser")
-teste = soup.find_all("table", class_="rankings-table")[0].find_all('tr')
+rankings_html = soup.find_all("table", class_="rankings-table")[0].find_all('tr')
 
-teams = []
-for e in teste[1:]:
+teams_hrefs = soup.find_all("table", class_="rankings-table")[0].find_all("a", class_="[ ellipsis ]")
+teams_urls = []
+for i in teams_hrefs:
+    link_url = i["href"]
+    teams_urls.append(url_base + link_url)
+
+new_page = requests.get(teams_urls[2])
+new_soup = BeautifulSoup(new_page.content, "html.parser")
+teste = new_soup.find_all("div", class_="[ column-group gutters ] communist")[0].text.split()
+
+defesas_index = teste.index("Defesas")
+medios_index = teste.index("Médios")
+avanc_index = teste.index("Avançados")
+nacionalidades = ["Portugal", "Brasil", "Ucrânia", "Suíça", "Grécia", "Bélgica", "Argentina", "Espanha", "Marrocos", "França", "Áustria", "Alemanha", "Uruguai", "Moçambique", "Ilhas de Cabo Verde", "República Democrática do Congo", "Guiné-Bissau", "Colômbia", "Sérvia", "México", "Irão", "Inglaterra", "Senegal", "Venezuela", "Japão", "Equador", "República Checa", "Hungria", "Mali", "Croácia", "Gana", "Costa do Marfim", "Camarões", "Israel", "Palestina", "Suécia", "Argélia", "Azerbaijão", "Eslovênia", "Canadá", "Chile"]
+gr = []
+defesas = []
+medios = []
+avanc = []
+
+cont = 1
+
+name = ""
+nac = ""
+position = ""
+
+while cont < len(teste):
+    if cont < defesas_index:
+        if (teste[cont] != "Guarda-Redes" and teste[cont] not in nacionalidades):
+            name += teste[cont] + " "    
+        if (teste[cont] in nacionalidades):
+            nac = teste[cont]
+        if teste[cont] == "Guarda-Redes":
+            position = teste[cont]
+        x = {
+            "name" : name,
+            "nacionality" : nac,
+            "position" : position
+        }
+        gr.append(x)
+    if defesas_index < cont < medios_index:
+        if (teste[cont] != "Defesa" and teste[cont] not in nacionalidades and teste[cont] != "Defesas"):
+            name += teste[cont] + " "    
+        if (teste[cont] in nacionalidades):
+            nac = teste[cont]
+        if teste[cont] == "Defesa":
+            position = teste[cont]
+        x = {
+            "name" : name,
+            "nacionality" : nac,
+            "position" : position
+        }
+        defesas.append(x)
+    if medios_index < cont < avanc_index:
+        if (teste[cont] != "Médio" and teste[cont] not in nacionalidades and teste[cont] != "Médios"):
+            name += teste[cont] + " "    
+        if (teste[cont] in nacionalidades):
+            nac = teste[cont]
+        if teste[cont] == "Médio":
+            position = teste[cont]
+        x = {
+            "name" : name,
+            "nacionality" : nac,
+            "position" : position
+        }
+        medios.append(x)
+    if avanc_index < cont:
+        if (teste[cont] != "Avançado" and teste[cont] not in nacionalidades and teste[cont] != "Avançados"):
+            name += teste[cont] + " "    
+        if (teste[cont] in nacionalidades):
+            nac = teste[cont]
+        if teste[cont] == "Avançado":
+            position = teste[cont]
+        x = {
+            "name" : name,
+            "nacionality" : nac,
+            "position" : position
+        }
+        avanc.append(x)
+    cont += 1
+
+# print(gr)
+print(teste)
+
+rankings = []
+for e in rankings_html[1:]:
     s = e.text.split()
     if s[1] == "Portimonense":
         x = {
@@ -24,7 +109,7 @@ for e in teste[1:]:
             "GA":s[8],
             "GD":s[9] 
         }
-        teams.append(x)
+        rankings.append(x)
     if s[1] == "Gil":
         x = {
             "pos": s[0],
@@ -38,7 +123,7 @@ for e in teste[1:]:
             "GA":s[10],
             "GD":s[11] 
         }
-        teams.append(x)
+        rankings.append(x)
     else:
         x = {
             "pos": s[0],
@@ -52,7 +137,7 @@ for e in teste[1:]:
             "GA":s[9],
             "GD":s[10] 
         }
-        teams.append(x)
+        rankings.append(x)
 
-with open("tables.json", "w") as outfile:
-    json.dump(teams, outfile)
+# with open("tables.json", "w") as outfile:
+#     json.dump(rankings, outfile)
